0179a411a0d43fb288677ec949018aab223721ee
dpetkov <dimiturpetkow@gmail.com>
2024-12-10 13:22:48 +0200

    FORMAT

--- terraform-rds/main.tf
+++ terraform-rds/main.tf
@@ -5,11 +5,11 @@ resource "aws_db_instance" "wordpress_db" {
   engine                  = "mysql"
   engine_version          = "8.0.30"
   instance_class          = "db.t2.micro"
-  db_name                    = "wordpress_db"
+  db_name                 = "wordpress_db"
   username                = var.db_username
   password                = var.db_password
   publicly_accessible     = false
-  multi_az                = false 
+  multi_az                = false
   backup_retention_period = 1
   skip_final_snapshot     = true
 }
